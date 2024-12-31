6c5460ed08844d6e34b8137e67e1723e64675d24
Distortedlogic <jermeek@gmail.com>
2024-12-31 13:59:50 -0500

    Update pagination.rs

--- frontend/maestro-hooks/src/pagination.rs
+++ frontend/maestro-hooks/src/pagination.rs
@@ -1,49 +1,15 @@
 use dioxus::prelude::*;
 
-#[derive(Clone, Debug, PartialEq)]
-pub enum PaginationError {
-	FailedToRead,
-	FailedToSet,
-	NotAvailable,
-}
-
 pub struct UsePagination {
 	pub idx: Signal<i32>,
-	pub total: Memo<i32>,
-	pub counter_label: Memo<String>,
 	pub page: Signal<i32>,
 	pub page_size: Signal<i32>,
-	pub can_next: Signal<bool>,
-	pub can_prev: Signal<bool>,
-}
 
-impl UsePagination {
-	pub fn next_index(&mut self) {
-		self.idx += 1;
-		let idx = *self.idx.read();
-		if idx != 0 && idx % *self.page_size.read() == 0 {
-			self.next_page();
-		}
-	}
-
-	pub fn prev_index(&mut self) {
-		self.idx -= 1;
-		let idx = *self.idx.read();
-		let page_size = *self.page_size.read();
-		if idx % page_size == page_size - 1 {
-			self.prev_page();
-		}
-	}
-
-	pub fn next_page(&mut self) {
-		self.page += 1;
-		self.can_next.set(*self.page.read() != *self.total.read());
-	}
-
-	pub fn prev_page(&mut self) {
-		self.page -= 1;
-		self.can_prev.set(*self.page.read() != 0);
-	}
+	pub counter_label: Memo<String>,
+	pub can_next_idx: Memo<bool>,
+	pub can_prev_idx: Memo<bool>,
+	pub can_next_page: Memo<bool>,
+	pub can_prev_page: Memo<bool>,
 }
 
 impl Clone for UsePagination {
@@ -54,15 +20,30 @@ impl Clone for UsePagination {
 
 impl Copy for UsePagination {}
 
-#[bon::builder]
-pub fn use_pagination(
-	total: Memo<i32>,
-	#[builder(with = |idx: i32| Signal::new(idx), default = Signal::new(0))] idx: Signal<i32>,
-	#[builder(with = |page: i32| Signal::new(page), default = Signal::new(1))] page: Signal<i32>,
-	#[builder(with = |page_size: i32| Signal::new(page_size), default = Signal::new(10))] page_size: Signal<i32>,
-	#[builder(with = |can_next: bool| Signal::new(can_next), default = Signal::new(true))] can_next: Signal<bool>,
-	#[builder(with = |can_prev: bool| Signal::new(can_prev), default = Signal::new(false))] can_prev: Signal<bool>,
-) -> UsePagination {
-	let counter_label = use_memo(move || format!("{} of {}", page, total));
-	UsePagination { idx, total, counter_label, page, page_size, can_next, can_prev }
+pub fn use_pagination(total: Memo<i32>) -> UsePagination {
+	let idx = use_signal(|| 0);
+	let mut page = use_signal(|| 1);
+	let page_size = use_signal(|| 10);
+	let mut touched = use_signal(|| false);
+	let last_page = use_memo(move || ((total() as f64) / (page_size() as f64)).ceil() as i32);
+	let can_next_idx = use_memo(move || idx() != total());
+	let can_prev_idx = use_memo(move || idx() != 0);
+	let can_next_page = use_memo(move || page() != last_page());
+	let can_prev_page = use_memo(move || page() != 1);
+	let counter_label = use_memo(move || format!("{} of {}", page(), total()));
+
+	use_effect(move || {
+		if idx() != 0 {
+			touched.set(true);
+		}
+	});
+	use_effect(move || {
+		if touched() && (idx() % page_size() == 0) {
+			page += 1;
+		}
+		if idx() > page_size() && idx() % page_size() == page_size() - 1 {
+			page -= 1;
+		}
+	});
+	UsePagination { idx, page, page_size, can_next_idx, can_prev_idx, can_next_page, can_prev_page, counter_label }
 }
