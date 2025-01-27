92107fe617c38b881ec08d5f1809c96eb53eeed6
Nataliia-Kholodkova <n_kholodkova@ukr.net>
2025-01-27 16:46:59 +0200

    update calendar

--- frontend/maestro-ui/src/calendar/mod.rs
+++ frontend/maestro-ui/src/calendar/mod.rs
@@ -14,10 +14,10 @@ use {
 pub mod enums;
 
 #[derive(Clone, PartialEq)]
-struct Event {
-	date: NaiveDate,
-	title: String,
-	color: Option<String>,
+pub struct Event {
+	pub date: NaiveDate,
+	pub title: String,
+	pub color: Option<String>,
 }
 
 #[derive(PartialEq, bon::Builder)]
@@ -44,8 +44,8 @@ impl Copy for CalendarDisplayProps {}
 pub struct CalendarSelectProps {
 	#[builder(default = Signal::new(chrono::Local::now().date_naive().month().into()))]
 	selected_month: Signal<ECalendarMonth>,
-	#[builder(default = Signal::new((chrono::Local::now().date_naive().weekday() as u8).into()))]
-	selected_day: Signal<ECalendarDay>,
+	#[builder(default = Signal::new(chrono::Local::now().date_naive().day() as u8))]
+	selected_day: Signal<u8>,
 	#[builder(default = Signal::new(chrono::Local::now().date_naive().year()))]
 	selected_year: Signal<i32>,
 	#[builder(default = Signal::new(None))]
@@ -87,6 +87,10 @@ pub fn Calendar(display_props: CalendarDisplayProps, select_props: CalendarSelec
 	let selected_date = use_memo(move || NaiveDate::from_ymd_opt(selected_year(), selected_month() as u32, selected_day() as u32).unwrap());
 	let state_props = CalendarStateProps { hover_date, selected_date };
 
+	let first_day = display_month().first_day(display_year());
+	let empty_cells = first_day.days_from_sunday();
+	let days_in_month = display_month().num_days(display_year());
+
 	rsx! {
 		CalendarMaybeWrapper { is_full, selected_date,
 			div { class: "w-full max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden calendar",
@@ -101,7 +105,15 @@ pub fn Calendar(display_props: CalendarDisplayProps, select_props: CalendarSelec
 							}
 						}
 						button {
-							onclick: move |_| display_month -= 1_u8.into(),
+							onclick: move |_| {
+									let prev_display_month = display_month() as u8;
+									display_month -= 1_u8.into();
+									if prev_display_month == 1 && display_month() as u8 == 12 {
+											display_year -= 1;
+									} else if prev_display_month == 12 && display_month() as u8 == 1 {
+											display_year += 1;
+									}
+							},
 							class: "p-1 rounded-full hover:bg-gray-200",
 							Icon {
 								class: "h-6 w-6 text-gray-600",
@@ -110,11 +122,19 @@ pub fn Calendar(display_props: CalendarDisplayProps, select_props: CalendarSelec
 						}
 					}
 					h2 { class: "text-lg font-semibold text-gray-800",
-						"{display_month()} {selected_year()}"
+						"{display_month()} {display_year()}"
 					}
 					div {
 						button {
-							onclick: move |_| display_month += 1_u8.into(),
+							onclick: move |_| {
+									let prev_display_month = display_month() as u8;
+									display_month += 1_u8.into();
+									if prev_display_month == 1 && display_month() as u8 == 12 {
+											display_year -= 1;
+									} else if prev_display_month == 12 && display_month() as u8 == 1 {
+											display_year += 1;
+									}
+							},
 							class: "p-1 rounded-full hover:bg-gray-200",
 							Icon {
 								class: "h-6 w-6 text-gray-600",
@@ -140,7 +160,15 @@ pub fn Calendar(display_props: CalendarDisplayProps, select_props: CalendarSelec
 						}
 					}
 					div { class: "grid grid-cols-7 gap-2 calendar-grid",
-						for delta in (0..=6 * 7) {
+						if empty_cells < 6 {
+							for _ in 0..=empty_cells {
+								div {
+									class: "h-10 w-10 rounded-full flex items-center justify-center text-sm text-gray-900 calendar-day",
+									class: "text-gray-400 cursor-not-allowed calendar-day__disabled",
+								}
+							}
+						}
+						for delta in (1..=days_in_month) {
 							CalendarDayComponent {
 								delta,
 								display_props,
@@ -198,48 +226,54 @@ pub fn CalendarMaybeWrapper(CalendarMaybeWrapperProps { is_full, children, selec
 
 #[component]
 pub fn CalendarDayComponent(delta: u8, display_props: CalendarDisplayProps, select_props: CalendarSelectProps, state_props: CalendarStateProps) -> Element {
-	let CalendarDisplayProps { display_month, display_year, .. } = display_props;
+	let CalendarDisplayProps { display_month, display_year, events, .. } = display_props;
 	let CalendarSelectProps { max_date, min_date, mut selected_day, mut selected_month, mut selected_year } = select_props;
 	let CalendarStateProps { mut hover_date, .. } = state_props;
 	let selected_date = use_memo(move || NaiveDate::from_ymd_opt(selected_year(), selected_month() as u32, selected_day() as u32).unwrap());
-	let first_day = display_month().first_day(display_year());
-	let last_day = display_month().last_day(display_year());
 
-	let is_before_first_day = delta <= first_day as u8;
-	let this_day = first_day - delta;
-	let is_after_last_day = last_day as u8 <= delta;
-	let day_is_in_display_month = !is_before_first_day && !is_after_last_day;
-	if !day_is_in_display_month {
+	let this_display_date = NaiveDate::from_ymd_opt(display_year(), display_month() as u32, delta.into());
+	if this_display_date.is_none() {
 		return rsx! {
-			button {
-				disabled: true,
+			div {
 				class: "h-10 w-10 rounded-full flex items-center justify-center text-sm text-gray-900 calendar-day",
 				class: "text-gray-400 cursor-not-allowed calendar-day__disabled",
-				"{this_day}"
+				""
 			}
 		};
 	}
-	let this_display_date = NaiveDate::from_ymd_opt(display_year(), display_month() as u32, delta.into()).unwrap();
+
+	let this_display_date = this_display_date.unwrap();
+	let curr_date_events = events.iter().filter(|e| e.date == this_display_date).collect::<Vec<_>>();
 	let is_today = this_display_date == chrono::Local::now().date_naive();
 	let date_smaller_than_min = min_date().map(|min_date| min_date > this_display_date).unwrap_or(false);
 	let date_bigger_than_max = max_date().map(|max_date| max_date < this_display_date).unwrap_or(false);
-	let is_disabled = !day_is_in_display_month || date_smaller_than_min || date_bigger_than_max;
+	let is_disabled = date_smaller_than_min || date_bigger_than_max;
 	rsx! {
 		button {
 			onclick: move |_| {
-					selected_day.set(this_day);
+					selected_day.set(delta);
 					selected_month.set(display_month());
 					selected_year.set(display_year());
 			},
 			onmouseenter: move |_| hover_date.set(Some(this_display_date)),
 			onmouseleave: move |_| hover_date.set(None),
 			disabled: is_disabled,
-			class: "h-10 w-10 rounded-full flex items-center justify-center text-sm text-gray-900 calendar-day",
+			class: "h-10 w-10 rounded-full flex items-center justify-center text-sm text-gray-900 calendar-day relative",
 			class: if is_today { "bg-blue-500 text-white calendar-day__today" } else { "" },
 			class: if selected_date() == this_display_date { "bg-blue-200 calendar-day__selected" } else { "" },
 			class: if is_disabled { "text-gray-400 cursor-not-allowed calendar-day__disabled" } else { "" },
 			class: if !is_today && selected_date() != this_display_date && !is_disabled { "hover:bg-gray-100" } else { "" },
-			"{this_day}"
+			"{delta}"
+			if !curr_date_events.is_empty() {
+				span { class: "absolute -bottom-0.5 left-0 right-0 mx-auto flex gap-0.5 items-center w-fit",
+					for e in curr_date_events {
+						span {
+							class: "flex w-1 h-1 rounded-full",
+							style: format!("background-color: {}", e.color.clone().unwrap_or(String::from("#3b82f6"))),
+						}
+					}
+				}
+			}
 		}
 	}
 }
